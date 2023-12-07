e7995cf66a663c72c4eab039a8d882beac6f932a
bektasc <bektascantokmak@gmail.com>
2023-12-07 17:57:01 +0300

    added colliders and new movement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,23 +1,47 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Joystick _joystick;
-
+    [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _movementSpeed;
+
+    private float _horizontal;
+    private float _vertical;
 
     private void Update()
     {
-        Movement();
+        GetMovementInputs();
     }
 
-    private void Movement()
+    private void FixedUpdate()
     {
+        SetMovement();
+    }
+
+    private void SetMovement()
+    {
+        Vector3 _vector3 = new(_horizontal, 0, _vertical);
+        Vector3 movementVector = _movementSpeed * Time.fixedDeltaTime * _vector3;
+        
+        _rb.velocity = movementVector;
+        if (movementVector != Vector3.zero)
+            transform.rotation = Quaternion.LookRotation(movementVector);
+    }
+
+    private void GetMovementInputs()
+    {
+        _horizontal = _joystick.Horizontal;
+        _vertical = _joystick.Vertical;
+    }
+    
+    /*private void Movement()
+    {  
         Vector3 movementVector = new Vector3(_joystick.Horizontal, 0, _joystick.Vertical);
 
         if (movementVector != Vector3.zero)
             transform.rotation = Quaternion.LookRotation(movementVector);
 
-        transform.position += _movementSpeed * Time.deltaTime * movementVector;
-    }
+        //transform.position += _movementSpeed * Time.deltaTime * movementVector;*/
 }
